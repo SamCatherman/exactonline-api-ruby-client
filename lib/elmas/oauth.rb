7bfc1b09b4bd1570992f430677b021171f2a1614
Samuel Catherman <samuel@Samuels-MBP.shiji.int>
2019-08-01 12:09:33 -0400

    temp puts to find type error class

--- lib/elmas/oauth.rb
+++ lib/elmas/oauth.rb
@@ -29,7 +29,7 @@ module Elmas
     def refresh_authorization
       warn "[DEPRECATION] `refresh_authorization` is deprecated. Please implement your own authorization methods instead."
       puts "refresh_token: #{refresh_token}"
-      puts "get_refresh_token: #{get_refresh_token(refresh_token).to_json.inspect}"
+      puts "get_refresh_token: #{get_refresh_token(refresh_token).to_json}"
       puts "get_refresh_token class: #{get_refresh_token(refresh_token).class}"
       OauthResponse.new(get_refresh_token(refresh_token).to_json).tap do |response|
         puts "inside oauth response block: #{response.inspect}"
